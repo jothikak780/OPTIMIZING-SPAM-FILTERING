bf5ee9c576be9e293e98ecc1481e16b3b0398b42
jothikak780 <127379453+jothikak780@users.noreply.github.com>
2023-04-20 13:17:46 +0530

    Add files via upload

--- OSF/python and flask file/app.py
+++ OSF/python and flask file/app.py
@@ -3,15 +3,16 @@ from flask import Flask, render_template, request
 import pickle
 import numpy as np
 import sklearn
-import warnings
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 
-warnings.filterwarnings('ignore')
 
 app = Flask(__name__)
 
-
-model = pickle.load(open('rf.pkl', 'rb'))
-
+model = pickle.load(open('rfm.pkl', 'rb'))
+cv = pickle.load(open('tfidf.pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -20,20 +21,22 @@ def home():
 
 @app.route('/getdata', methods=['POST'])
 def pred():
-    MSG = request.form['msg']
-    print(msg)
-    inp_features = [[msg]]
-    print(inp_features )
-    prediction = model.predict(inp_features)
+    message = request.form['msgs']
+    print(message)
+    inp_features = [message]
+    print(inp_features)
+    vect = cv.transform(inp_features).toarray()
+    prediction = model.predict(vect)
     print(type(prediction))
     t = prediction[0]
     print(t)
-    if t > 0.5:
-         prediction_text = 'SMS is SPAM'
+    if t == 0:
+        prediction_text = 'SMS is SPAM'
     else:
-         prediction_text = 'SMS is not SPAM'
+        prediction_text = 'SMS is not SPAM'
     print(prediction_text)
-    return render_template('prediction.html',prediction_results=prediction_text)
+    return render_template('prediction.html', prediction_results=prediction_text)
+
 
 if __name__ == "__main__":
     app.run()
